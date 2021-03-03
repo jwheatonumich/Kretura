a3024bd632f85a15e56257a2dfed12cfa8d7a089
James Heaton <jwheatonumich@gmail.com>
2021-03-03 16:33:57 -0600

    Added content to prebattle screen
    Prebattle screen no longer has sleep option
    Load text with level number

--- roguelike/prebattle-script.js
+++ roguelike/prebattle-script.js
@@ -1,10 +1,26 @@
 //Store data for the current level
-let currentLevel = enemySequence[playerStats["roguelike-level"]].level;
+let currentLevel = playerStats["roguelike-level"];
 let enemyList = enemySequence[playerStats["roguelike-level"]].enemyList;
 let enemyCount = enemySequence[playerStats["roguelike-level"]].enemyCount;
 
+//When fight button is clicked, start the fight
 document.getElementById('fight-button').onclick = function() {
-    startExplore(enemyList,2);
-    setMandatoryPage('../explore-game/explore.html')
+    startExplore(enemyList,enemyCount);
+    setMandatoryPage('../explore-game/explore.html');
 
-}
+};
+
+//Determine what shows-up in the text on page
+function determinePageText(){
+    let pageText = `Level: ${currentLevel}`;
+    return pageText;
+};
+
+//Function to update the text of an element on the page
+function updatePageText(text,ID){
+    gameTextPara = document.getElementById(ID);
+    gameTextPara.innerHTML = text;
+};
+
+pageText = determinePageText(); //Store the value that will go in the primary text on page
+window.onload = updatePageText(pageText,"game-text"); //On page load, update primary text on page
