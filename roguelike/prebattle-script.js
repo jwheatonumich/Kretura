68faa06ce635157de9a15f667d8622cd8d0fa3e9
James Heaton <jwheatonumich@gmail.com>
2021-03-10 11:45:26 -0600

    Replaced the explore screen with enemy selection on prebattle screen
    Added selectEnemies function to randomly select three of the possible enemies
    Added findEnemyByID function to find the location of each selected enemy in the enemyStats data
    Added enemyStats data to prebattle page
    Added updateEnemyChoiceImages function to add enemy images to html on the page
    Added updateEnemyChoiceLinks function to update the link around each enemy image to start a battle with that enemy

--- roguelike/prebattle-script.js
+++ roguelike/prebattle-script.js
@@ -40,6 +40,77 @@ function updateImage(image,ID){
     gameTextPara.src = image;
 };
 
+//Function to select three enemies that player can choose from
+function selectEnemies(){
+
+    let chosenEnemies = [];
+
+    for (let i = 0; i < 3; i++){
+
+        let randomEnemy = enemyList[Math.floor(Math.random()*enemyList.length)] //Select a random enemy from the list of possible enemies
+
+        chosenEnemies.push(randomEnemy);
+
+    };
+
+    return chosenEnemies;
+
+};
+
+function findEnemyById(inputId){
+
+    let outputId;
+
+    for (i in enemyStats){
+        if (enemyStats[i].enemyID === inputId){
+            outputId = i;
+        };
+    };
+
+    return outputId;
+}
+
+//Function to update enemy choice images
+function updateEnemyChoiceImages(){
+
+    //Store enemy image elements
+    enemy1Image = document.getElementById("enemy1-image");
+    enemy2Image = document.getElementById("enemy2-image");
+    enemy3Image = document.getElementById("enemy3-image");
+
+    //Store the file locations of enemy images
+    enemy1Src = enemyStats[findEnemyById(chosenEnemiesList[0])]["enemyImage"];
+    enemy2Src = enemyStats[findEnemyById(chosenEnemiesList[1])]["enemyImage"];
+    enemy3Src = enemyStats[findEnemyById(chosenEnemiesList[2])]["enemyImage"];
+
+    //Update image sources on page
+    enemy1Image.src = enemy1Src;
+    enemy2Image.src = enemy2Src;
+    enemy3Image.src = enemy3Src;
+
+}
+
+function updateEnemyChoiceLinks(){
+
+    //Store enemy IDs in arrays
+    enemy1OnClick = [enemyStats[findEnemyById(chosenEnemiesList[0])]["enemyID"]]
+    enemy2OnClick = [enemyStats[findEnemyById(chosenEnemiesList[1])]["enemyID"]]
+    enemy3OnClick = [enemyStats[findEnemyById(chosenEnemiesList[2])]["enemyID"]]
+
+    //Set on click of each enemy image to start battle with that enemy
+    document.getElementById('enemy1-link').onclick = function() {setMandatoryPage('roguelike/postbattle.html');startBattle(enemy1OnClick,false,true,true);};
+    document.getElementById('enemy2-link').onclick = function() {setMandatoryPage('roguelike/postbattle.html');startBattle(enemy2OnClick,false,true,true);};
+    document.getElementById('enemy3-link').onclick = function() {setMandatoryPage('roguelike/postbattle.html');startBattle(enemy3OnClick,false,true,true);};
+    
+}
+
+//Function to update battle start code with correct enemies
+function updateBattleStart(){
+
+
+
+}
+
 window.onload = setNextLevel();
 pageText = determinePageText(); //Store the value that will go in the primary text on page
 
@@ -47,3 +118,8 @@ pageText = determinePageText(); //Store the value that will go in the primary te
 window.onload = updatePageText(pageText,"game-text");
 window.onload = updatePageText(levelName,"level-name");
 window.onload = updateImage(pageImage,"page-image");
+
+//Populate the enemies for player to choose from
+let chosenEnemiesList = selectEnemies();
+window.onload = updateEnemyChoiceImages();
+window.onload = updateEnemyChoiceLinks();
