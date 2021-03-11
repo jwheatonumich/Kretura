8bce7a4ab11c20d486ef6197de9539bab570392a
James Heaton <jwheatonumich@gmail.com>
2021-03-10 18:26:39 -0600

    Minor enemy update

--- roguelike/enemy-sequence.js
+++ roguelike/enemy-sequence.js
@@ -27,6 +27,6 @@ enemySequence = [
     {level:26,levelName:"Deep Forest 1", levelImage:"../images/bountiful-tree.png",enemyList:[1,2,3],enemyCount:2, statReward:2, healReward:0},
     {level:27,levelName:"Deep Forest 2", levelImage:"../images/bountiful-tree.png",enemyList:[2,3,4],enemyCount:2, statReward:2, healReward:0},
     {level:28,levelName:"Deep Forest 3", levelImage:"../images/bountiful-tree.png",enemyList:[3,4],enemyCount:3, statReward:3, healReward:0},
-    {level:29,levelName:"Deep Forest 4", levelImage:"../images/bountiful-tree.png",enemyList:[4,5],enemyCount:3, statReward:3, healReward:0},
+    {level:29,levelName:"Deep Forest 4", levelImage:"../images/bountiful-tree.png",enemyList:[5],enemyCount:3, statReward:3, healReward:0},
     {level:30,levelName:"Spider's Lair 3", levelImage:"../images/bountiful-tree.png",enemyList:[25],enemyCount:1, statReward:5, healReward:1},
 ]
